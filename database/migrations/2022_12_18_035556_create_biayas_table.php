f23fbdbaab31d38c978f4707e19e6f0bf70a080d
GhozyIkbarF <ghozyikbarf@gmail.com>
2022-12-22 13:40:15 +0700

    install livewire

--- database/migrations/2022_12_18_035556_create_biayas_table.php
+++ database/migrations/2022_12_18_035556_create_biayas_table.php
@@ -15,7 +15,14 @@ return new class extends Migration
     {
         Schema::create('biayas', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('kegiatan', 100)->required();
+            $table->foreignId('nama_pegawai')->references('id')->on('pegawais')->onDelete('restrict');
+            $table->string('lokasi', 100)->required();
+            $table->date('hari_tgl', 100)->required();
+            $table->string('rekening')->required();
+            $table->string('uang_harian')->required();
+            $table->string('uang_transport')->required();
+            $table->string('biaya_transport')->required();
             $table->timestamps();
         });
     }
